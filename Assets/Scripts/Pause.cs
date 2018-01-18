d8dc86e7186b518cafc348f353bbe68391ab89b5
Buk7op <robxviktor@gmail.com>
2018-01-19 01:17:53 +0300

    Pause+ delay unpause+ background shadding

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Threading;
+
+public class Pause : MonoBehaviour {
+
+    public float timing;
+    public bool isPaused;
+    public GameObject menu, delaytxt, shade;
+	public Text delayPause; 
+	public bool dispDelay;
+	float time, difference;
+	void Start () {
+	}
+	
+	void Update () {
+
+		Time.timeScale = timing;
+		if (dispDelay == true) {
+			shade.SetActive (false);
+			menu.SetActive (false);
+			difference = 4 - (Time.realtimeSinceStartup - time);
+			delaytxt.SetActive (true);
+			delayPause.text = "" + (int)difference;
+		} 
+		else if (dispDelay == false)
+		{
+			delaytxt.SetActive (false);
+		}
+
+		if(isPaused == true)
+        {
+			shade.SetActive (true);
+			menu.SetActive (true);
+            timing = 0;
+        }
+		else if(isPaused == false && difference < 1.05)
+        {
+			shade.SetActive (false);
+            timing = 1;
+			menu.SetActive (false);
+			dispDelay = false;
+        }
+	}
+
+	public void PauseButton(bool statePause) {
+		if (statePause == false) {
+			time = Time.realtimeSinceStartup;
+			isPaused = false;
+			dispDelay = true;
+			} 
+		else
+		{
+		isPaused = true;
+		dispDelay = false;
+		}
+	}
+}
--- Assets/Scripts/drag1.cs
+++ Assets/Scripts/drag1.cs
@@ -7,12 +7,14 @@ public class drag1 : MonoBehaviour {
 	public Transform player;
 
 	void OnMouseDrag () {
-		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		mousePos.x = mousePos.x > 2.7f ? 2.7f : mousePos.x;
-		mousePos.x = mousePos.x < -2.7f ? -2.7f : mousePos.x;
-		mousePos.y = mousePos.y > 3.6f ? 3.6f : mousePos.y;
-		mousePos.y = mousePos.y < -4.9f ? -4.9f : mousePos.y;
-		player.position = new Vector2 (mousePos.x, mousePos.y);
+		if (Time.timeScale != 0) {
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			mousePos.x = mousePos.x > 2.7f ? 2.7f : mousePos.x;
+			mousePos.x = mousePos.x < -2.7f ? -2.7f : mousePos.x;
+			mousePos.y = mousePos.y > 3.6f ? 3.6f : mousePos.y;
+			mousePos.y = mousePos.y < -4.9f ? -4.9f : mousePos.y;
+			player.position = new Vector2 (mousePos.x, mousePos.y);
+		}
 	}
 
 }
