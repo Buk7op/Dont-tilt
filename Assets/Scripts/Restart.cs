63e4efe9b9358eea1f8d4d725dd660219a8d6ebe
Buk7op <robxviktor@gmail.com>
2018-01-19 17:04:07 +0300

    Settings menu(not yet)

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+	public bool RestartDown;
+	private Scene scene;
+	// Use this for initialization
+	void Start () {
+		scene = SceneManager.GetActiveScene ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(RestartDown == true)
+		{
+			SceneManager.LoadScene (scene.name);
+			RestartDown = false;
+		}
+	}
+	public void SettingButton(bool buttonDown) {
+		RestartDown = buttonDown;
+	}
+}
