512452733a412f58f1e9b5752f2ffa811298ac76
Buk7op <robxviktor@gmail.com>
2018-01-19 20:14:58 +0300

    Enable/disable button

---
+++ Assets/Scripts/DisableEnable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableEnable : MonoBehaviour {
+	public Button button;
+	public Sprite enabled, disabled;
+	public bool status;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<Button> ();
+	}
+
+	void Update (){
+		if (status == true)
+			button.image.overrideSprite = disabled;
+		else
+			button.image.overrideSprite = enabled;
+		
+	}
+
+public void SettingButton(bool buttonDown) {
+		if (status == false)
+			status = buttonDown;
+		else
+			status = !buttonDown;
+}
+}
